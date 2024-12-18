1aace11e135aea94c1a40362b6b41a6b927815dd
zelawest <169395273+ZELAWEST@users.noreply.github.com>
2024-12-18 21:40:07 +0200

    Change Logic to Object Constructors

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ let myLibrary = [
     { title: "The Logic", author: "Dr Philly", pages: "324", read: "read" },
     { title: "Real Men", author: "Morgan Alonxo", pages: "296", read: "Not Yet Read" },
     { title: "The Bush Baby", author: "Phindele Zoko", pages: "188", read: "read" },
-    { title: "Ambulance", author: "Dr Mokhosi", pages: "604", read: "Not Yet Read" }, 
+    { title: "Ambulance", author: "Dr Mokhosi", pages: "604", read: "Not Yet Read" },
     { title: "Math", author: "Phindele Zoko", pages: "188", read: "read" },
     { title: "Science", author: "Dr Mokhosi", pages: "604", read: "Not Yet Read" }
-   
+
 
 ]
 
@@ -40,124 +40,132 @@ Book.prototype.info = function () {
     return `The ${this.title} by ${this.author}, ${this.pages} pages, ${read}`
 
 }
-// 
-function addBookToLibrary(title, author, pages, read) {
-
-    const bookObj = new Book(title, author, pages, read)
-    myLibrary.push(bookObj)
-
-}
-function displayBooks() {
-    myLibrary.forEach((book, index) => {
-        createCard(book, index)
-    })
-}
-function createCardButton(card) {
-    const button = document.createElement('button')
-
-    button.style.backgroundColor = "transparent"
-    button.style.color = card.style.backgroundColor
-    button.setAttribute('class', 'card-buttons')
+function Library() {
 
 
-    return button
+    this.createCardButton = function (card) {
+        const button = document.createElement('button')
 
-}
-function createCard(book, index) {
-    const card = document.createElement('div')
-    card.setAttribute('id', index)
-    card.setAttribute("class", "card")
-
-    const header = document.createElement('h4')
-    header.textContent = book.title
+        button.style.backgroundColor = "transparent"
+        button.style.color = card.style.backgroundColor
+        button.setAttribute('class', 'card-buttons')
 
 
-    const author = document.createElement('p')
-    author.textContent = book.author
-    const pages = document.createElement('p')
-    pages.textContent = book.pages
-    const read = document.createElement('p')
-    read.textContent = book.read
+        return button
 
-    author.style.color = maincontainer.style.backgroundColor
-    pages.style.color = maincontainer.style.backgroundColor
-    read.style.color = maincontainer.style.backgroundColor
+    }
+    this.addBookToLibrary = function (title, author, pages, read) {
 
+        const bookObj = new Book(title, author, pages, read)
+        myLibrary.push(bookObj)
 
-    // Card Labels
-    const titleLabel = document.createElement('label')
-    const authorLabel = document.createElement('label')
-    const readLabel = document.createElement('label')
-    const pagesLabel = document.createElement('label')
-    titleLabel.textContent = "Title"
-    authorLabel.textContent = "Author"
-    readLabel.textContent = "Read"
-    pagesLabel.textContent = "Pages"
+    }
+    this.displayBooks = function () {
+        myLibrary.forEach((book, index) => {
+            this.createCard(book, index)
+        })
 
-    titleLabel.style.backgroundColor = maincontainer.style.backgroundColor
-    authorLabel.style.backgroundColor = maincontainer.style.backgroundColor
-    readLabel.style.backgroundColor = maincontainer.style.backgroundColor
-    pagesLabel.style.backgroundColor = maincontainer.style.backgroundColor
+    }
+    this.createCard = function (book, index) {
+        const card = document.createElement('div')
+        card.setAttribute('id', index)
+        card.setAttribute("class", "card")
 
-    const lables = document.createElement('div')
-    lables.setAttribute('class', 'bookInfo')
+        const header = document.createElement('h4')
+        header.textContent = book.title
 
 
-    lables.append(authorLabel)
-    lables.append(author)
-    lables.append(pagesLabel)
-    lables.append(pages)
-    lables.append(readLabel)
-    lables.append(read)
-    card.append(header)
-    card.append(lables)
-
-    const divButtons = document.createElement('div')
-    divButtons.setAttribute("class", "buttons")
-
-    const changeReadStatus = createCardButton(card)
-
-    changeReadStatus.textContent = "Read Or Not Yet"
-    changeReadStatus.addEventListener('click', () => {
-
-        book.read = book.read.toLowerCase() === 'read' ? 'Not Yet Read' : 'Read'
+        const author = document.createElement('p')
+        author.textContent = book.author
+        const pages = document.createElement('p')
+        pages.textContent = book.pages
+        const read = document.createElement('p')
         read.textContent = book.read
-    })
-    divButtons.append(changeReadStatus)
 
-    const deleteBook = createCardButton(card)
-    deleteBook.textContent = "Delete"
-    deleteBook.addEventListener('click', () => {
+        
+        read.style.color = author.style.color = pages.style.color = maincontainer.style.backgroundColor
+       
 
 
-        const id = card.id
+        // Card Labels
+        const titleLabel = document.createElement('label')
+        const authorLabel = document.createElement('label')
+        const readLabel = document.createElement('label')
+        const pagesLabel = document.createElement('label')
+        titleLabel.textContent = "Title"
+        authorLabel.textContent = "Author"
+        readLabel.textContent = "Read"
+        pagesLabel.textContent = "Pages"
 
-        const mainchildren = maincontainer.querySelectorAll('.card')
-        const mainCards = Array.from(mainchildren)
-        myLibrary.filter((item, index) => {
-            if (mainCards[index].id === id) {
-                myLibrary.splice(id, 1)
 
-            }
+        readLabel.style.backgroundColor = pagesLabel.style.backgroundColor = titleLabel.style.backgroundColor = authorLabel.style.backgroundColor = maincontainer.style.backgroundColor
+
+
+
+        const lables = document.createElement('div')
+        lables.setAttribute('class', 'bookInfo')
+
+
+        lables.append(authorLabel)
+        lables.append(author)
+        lables.append(pagesLabel)
+        lables.append(pages)
+        lables.append(readLabel)
+        lables.append(read)
+        card.append(header)
+        card.append(lables)
+
+        const divButtons = document.createElement('div')
+        divButtons.setAttribute("class", "buttons")
+console.log(this)
+        const changeReadStatus = this.createCardButton(card)
+
+        changeReadStatus.textContent = "Read Or Not Yet"
+        changeReadStatus.addEventListener('click', () => {
+
+            book.read = book.read.toLowerCase() === 'read' ? 'Not Yet Read' : 'Read'
+            read.textContent = book.read
+        })
+        divButtons.append(changeReadStatus)
+
+        const deleteBookBtn = this.createCardButton(card)
+        deleteBookBtn.textContent = "Delete"
+        deleteBookBtn.addEventListener('click', () => {
+
+
+            const id = card.id
+
+            const mainchildren = maincontainer.querySelectorAll('.card')
+            const mainCards = Array.from(mainchildren)
+            myLibrary.filter((item, index) => {
+                if (mainCards[index].id === id) {
+                    myLibrary.splice(id, 1)
+
+                }
+
+            })
+            this.deleteCards(maincontainer)
+            this.displayBooks()
 
         })
-        deleteCards(maincontainer)
-        displayBooks()
-
-    })
 
 
 
-    divButtons.append(deleteBook)
-    card.append(divButtons)
-    maincontainer.append(card)
+        divButtons.append(deleteBookBtn)
+        card.append(divButtons)
+        maincontainer.append(card)
 
+    }
+    this.deleteCards = function (cardContainer) {
+        while (cardContainer.hasChildNodes()) {
+            cardContainer.removeChild(cardContainer.firstChild);
+        }
+
+    }
 }
+let library = new Library()
 
-
-displayBooks()
-
-
+library.displayBooks()
 
 addBook.addEventListener("click", function () {
 
@@ -180,20 +188,14 @@ popupAddBook.addEventListener('click', (e) => {
     if (title === '' || author === '' || pages === '' || read === '') {
         alert("Please enter correct info")
     } else {
-        deleteCards(maincontainer)
-        addBookToLibrary(title, author, pages, read)
-        displayBooks()
+        library.deleteCards(maincontainer)
+        library.addBookToLibrary(title, author, pages, read)
+        library.displayBooks()
 
-        titleText.value = '' 
-         authorText.value = ''
-         pagesText.value = ''
-         readText.value = ''
+        titleText.value = ''
+        authorText.value = ''
+        pagesText.value = ''
+        readText.value = ''
         dialog.classList.remove('show')
     }
 })
-function deleteCards(cardContainer) {
-    while (cardContainer.hasChildNodes()) {
-        cardContainer.removeChild(cardContainer.firstChild);
-    }
-
-}
